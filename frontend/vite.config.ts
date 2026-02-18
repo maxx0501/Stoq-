357c32877b40a0a5e8b70305aa5c8eb933abd6b4
Maxx0501 <mateused0501@gmail.com>
2026-02-18 10:50:11 -0300

    acho que agora vai ein

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Garante que os links funcionem na raiz
+  build: {
+    outDir: 'dist', // Força a saída para a pasta 'dist'
+  }
 })
